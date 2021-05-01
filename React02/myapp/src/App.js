1585349c7c50d9e7881371a823d27cce0e8c6542
toannd071097 <toannd071097@gmail.com>
2021-05-01 17:05:41 +0700

    cập nhật Clock

--- React02/myapp/src/App.js
+++ React02/myapp/src/App.js
@@ -15,13 +15,29 @@ export default class App extends React.Component {
 }
 
 class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: new Date().toLocaleString(),
+    };
+    this.updateTimer();
+  }
+
+  updateTimer = () => {
+    setInterval(() => {
+      this.setState({
+        timer: new Date().toLocaleString(),
+      });
+    }, 1000);
+  };
+
   render() {
+    const { timer } = this.state;
     return (
       <div>
         <h3>This is timer in VN</h3>
-        <p>{ new Date().toLocaleString() }</p>
+        <p>{timer}</p>
       </div>
     );
   }
 }
-
